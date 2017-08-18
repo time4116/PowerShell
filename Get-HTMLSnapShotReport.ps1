4d1be6c4b26c0a5564b62851beea19ff5178627e
time4116 <31143754+time4116@users.noreply.github.com>
2017-08-18 16:26:47 -0700

    Update Get-HTMLSnapShotReport.ps1

--- Get-HTMLSnapShotReport.ps1
+++ Get-HTMLSnapShotReport.ps1
@@ -2,10 +2,8 @@
 .Synopsis
    Short function to gather VMware snapshots metrics and send an email report.
 
-
 .DESCRIPTION
-   
-
+   Coming soon
 
 .EXAMPLE
     Get-HTMLSnapShotReport -VIServer vcenter.yourdomain.local -SmtpServer blah@yourdomain.com -to blah@yourdomain.com -from blahblah@yourdomain.com
